f41c7265b81aaf4de8126707a1ca280a3c2a0634
EHMahi9 <vaibongo20@gmail.com>
2026-09-11 12:47:02 +0600

    Flatten Assignment 05 application structure

--- Assignments/Assignment-05/B14-A05-DevStack/Assignment-05/Assignment-05/Assignment-05/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// এখানে আমরা tailwind-এর প্লাগিনটি সরিয়ে দিলাম, কারণ Version 3-তে এটি লাগে না
-export default defineConfig({
-  plugins: [react()],
-})
